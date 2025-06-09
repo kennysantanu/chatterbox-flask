73a4edfa51c04ca8d489d7ba97353dc3d70065f1
Kenny Santanu <31720797+kennysantanu@users.noreply.github.com>
2025-06-08 21:59:17 -0700

    Add environment variable support

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import io
+import os
 import torch
 import torchaudio
+from dotenv import load_dotenv
 from chatterbox.tts import ChatterboxTTS
 from flask import Flask, request, jsonify, Response
 
 app = Flask(__name__)
 
-# Local variables
-audio_prompt_path = ""
-exaggeration = 0.7
-cfg_weight = 0.9
-temperature = 0.5
+# Load environment variables
+load_dotenv()
+prompt_file = os.getenv('AUDIO_PROMPT', 'sample.mp3')
+audio_prompt_path = os.path.join('audio_prompt', prompt_file)
+exaggeration = float(os.getenv('EXAGGERATION', 0.5))
+cfg_weight = float(os.getenv('CFG_WEIGHT', 0.5))
+temperature = float(os.getenv('TEMPERATURE', 0.5))
 
 # Initialize the Chatterbox TTS model
 if torch.cuda.is_available():
@@ -21,8 +25,11 @@ else:
     device = "cpu"
 
 model = ChatterboxTTS.from_pretrained(device=device)
-if audio_prompt_path:
+if os.path.isfile(audio_prompt_path):
     model.prepare_conditionals(wav_fpath=audio_prompt_path, exaggeration=exaggeration)
+else:
+    if prompt_file:
+        print(f"Audio prompt file '{audio_prompt_path}' not found. Use default voice.")
 
 # Endpoint for text-to-speech
 @app.route('/v1/tts', methods=['POST'])
@@ -31,17 +38,20 @@ def tts():
     if not data or 'text' not in data:
         return jsonify({'error': 'No text provided'}), 400
     text = data['text']
-
-    # Generate audio from text
-    wav = model.generate(text, exaggeration=exaggeration, cfg_weight=cfg_weight, temperature=temperature)
-    buffer = io.BytesIO()
-    torchaudio.save(buffer, wav, model.sr, format='wav')
-    buffer.seek(0)
-
-    # Return the audio file as a response
-    return Response(buffer.read(), mimetype='audio/wav', headers={
-        'Content-Disposition': 'attachment; filename=output.wav'
-    })
+    try:
+        # Generate audio from text
+        buffer = io.BytesIO()
+        wav = model.generate(text, exaggeration=exaggeration, cfg_weight=cfg_weight, temperature=temperature)
+        torchaudio.save(buffer, wav, model.sr, format='wav')
+        buffer.seek(0)
+        
+        # Return the audio file as a response
+        return Response(buffer.read(), mimetype='audio/wav', headers={
+            'Content-Disposition': 'attachment; filename=output.wav'
+        })
+    except Exception as e:
+        print("TTS generation failed.")
+        return jsonify({'error': f'TTS generation failed: {str(e)}'}), 500
     
 if __name__ == '__main__':
     app.run(debug=True)
