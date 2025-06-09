e2185bcd41bcabfc1c7d36c629dd41bb118f370a
Kenny Santanu <31720797+kennysantanu@users.noreply.github.com>
2025-06-09 00:22:35 -0700

    Feat: Enable CPU/MPS support

--- main.py
+++ main.py
@@ -25,6 +25,16 @@ elif torch.backends.mps.is_available():
 else:
     device = "cpu"
 
+map_location = torch.device(device)
+
+torch_load_original = torch.load
+def patched_torch_load(*args, **kwargs):
+    if 'map_location' not in kwargs:
+        kwargs['map_location'] = map_location
+    return torch_load_original(*args, **kwargs)
+
+torch.load = patched_torch_load
+
 model = ChatterboxTTS.from_pretrained(device=device)
 if os.path.isfile(audio_prompt_path):
     model.prepare_conditionals(wav_fpath=audio_prompt_path, exaggeration=exaggeration)
